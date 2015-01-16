10259c28bac4e446af62fd48c814c769b80e111d
Ariel Rodriguez <ariel@volonbolon.net>
2015-01-16 12:44:31 -0300

    Add Xcode Project

---
+++ ShinyOctoCyril/ShinyOctoCyril/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ShinyOctoCyril
+//
+//  Created by Ariel Rodriguez on 1/16/15.
+//  Copyright (c) 2015 Ariel Rodriguez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ShinyOctoCyril/ShinyOctoCyril/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  ShinyOctoCyril
+//
+//  Created by Ariel Rodriguez on 1/16/15.
+//  Copyright (c) 2015 Ariel Rodriguez. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
