02fb5305913f35b4a95834395ce735e489c53518
Ariel Rodriguez <ariel@volonbolon.net>
2015-01-23 16:32:25 -0300

    Add documentation

--- ShinyOctoCyril/ShinyOctoCyril/NSLayoutConstraint+VBSelfInstall.h
+++ ShinyOctoCyril/ShinyOctoCyril/NSLayoutConstraint+VBSelfInstall.h
@@ -9,5 +9,30 @@
 #import <UIKit/UIKit.h>
 
 @interface NSLayoutConstraint (VBSelfInstall)
+/**
+ *  Install the constraint that receive the message in the appropriate view
+ *  The method walk the view hiearchy to find the common ancestor
+ *  of the items related by the constraint.
+ *
+ * The constraint priority is set to `UILayoutPriorityRequired`
+ *
+ *  @return `YES` on success
+ */
+- (BOOL)vb_install;
 
+/**
+ *  Install the constraint in the appropriate view, like `-bsb_install`,
+ *  but let the client configure the priority
+ *
+ *  @param priority Priority level used to install the constraint
+ *
+ *  @return `YES` on success
+ */
+- (BOOL)vb_install:(float)priority;
+
+/**
+ *  Walks the view hiearchy to find the commom ancestor
+ *  where the constraint was installed, and remove it
+ */
+- (void)vb_remove;
 @end
