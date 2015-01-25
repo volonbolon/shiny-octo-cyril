addabb9abe249f7db7b58aabe5b9cd19dbd05156
Ariel Rodriguez <ariel@volonbolon.net>
2015-01-25 11:50:09 -0300

    Add FWKControllerForGrid Protocol

---
+++ ShinyOctoCyril/ShinyOctoCyril/Grid/FWKControllerForGrid.h
@@ -0,0 +1,14 @@
+//
+//  FWKControllerForGrid.h
+//  ShinyOctoCyril
+//
+//  Created by Ariel Rodriguez on 1/25/15.
+//  Copyright (c) 2015 Ariel Rodriguez. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol FWKControllerForGrid <NSObject>
+@required
+- (UIViewController *)currentDetailViewController;
+@end
